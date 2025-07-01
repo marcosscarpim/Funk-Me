fbc33eec546bd79881d21ef767a05c021cae7c3b
Marcos Scarpim <a55149@MacBook-Pro.local>
2025-07-01 10:14:46 -0300

    Fix FunkScreen drawing below bars

--- app/src/main/java/com/scarpim/funkme/MainActivity.kt
+++ app/src/main/java/com/scarpim/funkme/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.scarpim.funkme
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import com.scarpim.funkme.funkScreen.FunkScreen
 import com.scarpim.funkme.funkScreen.FunkScreenAction
 import com.scarpim.funkme.funkScreen.FunkViewModel
@@ -26,13 +35,37 @@ class MainActivity : ComponentActivity() {
             FunkMeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FunkScreen(viewModel = viewModel)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(WindowInsets.safeDrawing.asPaddingValues())
+                    ) {
+                        FunkScreen(viewModel = viewModel)
+                    }
                 }
             }
         }
         viewModel.onAction(FunkScreenAction.LoadAudios)
+    }
+
+    private fun setStatusBarColor(window: Window, color: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) { // Android 15+
+            window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+                val statusBarInsets = insets.getInsets(android.view.WindowInsets.Type.statusBars())
+                val navBarInsets = insets.getInsets(android.view.WindowInsets.Type.navigationBars())
+                view.setBackgroundColor(color)
+
+                // Adjust padding to avoid overlap
+                view.setPadding(0, statusBarInsets.top, 0, navBarInsets.bottom)
+                insets
+            }
+        } else {
+            // For Android 14 and below
+            window.statusBarColor = color
+        }
     }
 }
